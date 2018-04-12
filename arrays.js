5e04ceb9002231e7c54e5544b7bfbd83d7a16ef9
Amy Tu <siamesebb@gmail.com>
2018-04-12 01:35:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var string = "foo"
 function addElementToBeginningOfArray(chocolateBars, string){
   return [string, ...chocolateBars]
 }
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(chocolateBars, string){
   chocolateBars.unshift("foo")
   return chocolateBars
@@ -32,3 +33,5 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
 function removeElementFromEndOfArray(chocolateBars){
   return chocolateBars.slice(0, chocolateBars.length - 1)
 }
+=======
+>>>>>>> 008d79b4366a9c9f00265beb8b1862383c57ff1f
